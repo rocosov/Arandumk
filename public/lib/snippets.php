0415520adf4ae89773b633332df78b796d83bd68
rocosov <ps2carlos@gmail.com>
2019-05-17 08:07:31 -0400

    detalles

--- public/lib/snippets.php
+++ public/lib/snippets.php
@@ -190,9 +190,9 @@ function guia_de_posts1(){
                <div class="pagination pagination-right">
                  <ul>
                    <li><a href="#">Prev</a></li>
-                   <li><a href="#">1</a></li>
-                   <li class="active"><a href="#">2</a></li>
-                   <li><a href="#">3</a></li>
+				   <li class="active"><a href="#">1</a></li>
+                   <li><a href="#">2</a></li>
+				   <li><a href="#">3</a></li>
                    <li><a href="#">4</a></li>
                    <li><a href="#">Next</a></li>
                  </ul>
@@ -253,8 +253,8 @@ function guia_de_posts3(){
        <div class="pagination">
          <ul>
            <li><a href="#">Prev</a></li>
-           <li><a href="#">1</a></li>
-           <li class="active"><a href="#">2</a></li>
+		   <li class="active"><a href="#">1</a></li>
+           <li><a href="#">2</a></li>
            <li><a href="#">3</a></li>
            <li><a href="#">4</a></li>
            <li><a href="#">Next</a></li>
